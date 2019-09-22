65750a8dead3e54c0437115e943e4e91c9ff61fb
kanonair <15954027738@163.com>
2019-09-22 13:50:05 +0800

    init

---
+++ JavaSE/src/main/java/io/kanonair/LambdaDemo.java
@@ -0,0 +1,41 @@
+package io.kanonair;
+
+import org.junit.Test;
+
+import java.util.Comparator;
+
+public class LambdaDemo {
+
+    /**
+     * 原始方式
+     */
+    @Test
+    public void example01() {
+        Runnable r1 = new Runnable() {
+            public void run() {
+                System.out.println("example01");
+            }
+        };
+        r1.run();
+    }
+
+    /**
+     * Lambda表达式
+     */
+    @Test
+    public void example02() {
+        Runnable r2 = () -> System.out.println("example02");
+        r2.run();
+    }
+
+    /**
+     * Lambda表达式 方法引用
+     */
+    @Test
+    public void example03() {
+        Comparator<Integer> c1 = Integer::compareTo;
+        int i = c1.compare(1, 2);
+        System.out.println("i = " + i);
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-https://www.bilibili.com/video/av48144058/?p=664
+    Lambda表达式
+    函数式接口
+    方法引用
+    Stream API
+    Optional类
