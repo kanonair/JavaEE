52480e7e443226799f9fe4372b5f23ad40deab00
kanonair <15954027738@163.com>
2019-09-23 13:22:58 +0800

    init

--- JavaSE/src/main/java/io/kanonair/reflect/ReflectDemo.java
+++ JavaSE/src/main/java/io/kanonair/reflect/ReflectDemo.java
@@ -2,14 +2,52 @@ package io.kanonair.reflect;
 
 import org.junit.Test;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 public class ReflectDemo {
 
+    /**
+     * 公有
+     */
     @Test
-    public void example01() {
-        Person p1 = new Person(18, "Taylor");
-        p1.age = 20;
-        System.out.println(p1);
-        p1.say();
+    public void example01() throws Exception {
+        Class<Person> pClass = Person.class;
+        // 构造器
+        Constructor<Person> pConstructor = pClass.getDeclaredConstructor(Integer.class, String.class);
+        Person person = pConstructor.newInstance(18, "Taylor");
+        System.out.println(person);
+        // 属性
+        Field age = pClass.getDeclaredField("age");
+        age.set(person, 20);
+        System.out.println(person);
+        // 方法
+        Method say = pClass.getDeclaredMethod("say");
+        say.invoke(person);
+    }
+
+    /**
+     * 私有
+     */
+    @Test
+    public void example02() throws Exception {
+        Class<Person> pClass = Person.class;
+        // 构造器
+        Constructor<Person> pConstructor = pClass.getDeclaredConstructor(String.class);
+        pConstructor.setAccessible(true);
+        Person person = pConstructor.newInstance("Tom");
+        System.out.println(person);
+        // 属性
+        Field name = pClass.getDeclaredField("name");
+        name.setAccessible(true);
+        name.set(person, "Jack");
+        System.out.println(person);
+        // 方法
+        Method showSex = pClass.getDeclaredMethod("showSex", String.class);
+        showSex.setAccessible(true);
+        Object invoke = showSex.invoke(person, "男");
+        System.out.println(invoke);
     }
 
 }
