77f49ff490e814d3605281a80a3eb398fd4dfafa
kanonair <15954027738@163.com>
2019-09-26 02:47:37 +0800

    init

--- JavaSE/src/main/java/io/kanonair/file/FileDemo.java
+++ JavaSE/src/main/java/io/kanonair/file/FileDemo.java
@@ -2,11 +2,14 @@ package io.kanonair.file;
 
 import org.junit.Test;
 
+import java.io.File;
+
 public class FileDemo {
 
     @Test
     public void example01() {
-
+        File file = new File("jdbc.properties");
+        System.out.println(file.getAbsolutePath());
     }
 
 }
