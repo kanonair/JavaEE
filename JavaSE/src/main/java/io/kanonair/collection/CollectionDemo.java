58f407264c36a4dd3c88bffd85052007b171f523
kanonair <15954027738@163.com>
2019-09-29 17:04:56 +0800

    init

--- JavaSE/src/main/java/io/kanonair/collection/CollectionDemo.java
+++ JavaSE/src/main/java/io/kanonair/collection/CollectionDemo.java
@@ -2,10 +2,30 @@ package io.kanonair.collection;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
 public class CollectionDemo {
 
+    /**
+     * Collection
+     */
     @Test
     public void example01() {
-
+        Collection<Person> col = new ArrayList<>();
+        // 添加
+        col.add(new Person(18, "Taylor"));
+        col.add(new Person(19, "Taylor"));
+        col.add(new Person(20, "Taylor"));
+        System.out.println(col);
+        // 包含
+        System.out.println(col.contains(new Person(18, "Taylor")));
+        boolean containsAll = col.containsAll(
+                Arrays.asList(new Person(19, "Taylor"), new Person(20, "Taylor")));
+        System.out.println(containsAll);
+        // 移除
+        col.remove(new Person(19, "Taylor"));
+        System.out.println(col);
     }
 }
