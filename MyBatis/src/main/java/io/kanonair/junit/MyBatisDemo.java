0104ab04eaaae5dd47e0f325809fd17c237657b5
kanonair <15954027738@163.com>
2019-10-15 23:31:04 +0800

    init

--- MyBatis/src/main/java/io/kanonair/junit/MyBatisDemo.java
+++ MyBatis/src/main/java/io/kanonair/junit/MyBatisDemo.java
@@ -6,22 +6,115 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MyBatisDemo {
 
-    @Test
-    public void example01() throws Exception {
+    private SqlSession sqlSession;
+
+    @Before
+    public void before() throws Exception {
         String resource = "mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-        SqlSession sqlSession = sqlSessionFactory.openSession();
+        sqlSession = sqlSessionFactory.openSession();
+    }
+
+    /**
+     * 单个参数
+     */
+    @Test
+    public void example01() {
         UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
-        List<User> userList = userMapper.selectAll();
-        userList.forEach(System.out::println);
+        User user = userMapper.selectById(1);
+        System.out.println(user);
+    }
+
+    /**
+     * 多个参数
+     */
+    @Test
+    public void example02() {
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        List<User> list = userMapper.selectByNameAndSex("Taylor", 0);
+        list.forEach(System.out::println);
+    }
+
+    /**
+     * Map 参数
+     */
+    @Test
+    public void example03() {
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Taylor");
+        map.put("sex", "0");
+        List<User> list = userMapper.selectByNameAndSexMap(map);
+        list.forEach(System.out::println);
+    }
+
+    /**
+     * javabean
+     */
+    @Test
+    public void example04() {
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        User user = new User();
+        user.setId(10);
+        user.setName("kanonair");
+        user.setSex(1);
+        userMapper.update(user);
+        sqlSession.commit();
+    }
+
+    /**
+     * 多个javabean
+     */
+    @Test
+    public void example05() {
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        User u1 = new User();
+        u1.setName("Taylor");
+        User u2 = new User();
+        u2.setSex(0);
+        List<User> list = userMapper.selectByUserTwo(u1, u2);
+        list.forEach(System.out::println);
+    }
+
+    /**
+     * 模糊查询
+     */
+    @Test
+    public void example06() {
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        List<User> list = userMapper.selectLikeName("T");
+        list.forEach(System.out::println);
+    }
+
+    /**
+     * 返回自增主键
+     */
+    @Test
+    public void example09() {
+        // useGeneratedKeys="true" keyProperty="id"
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        User user = new User();
+        user.setName("Taylor");
+        user.setSex(0);
+        userMapper.insert(user);
+        sqlSession.commit();
+        System.out.println(user);
+    }
+
+    @After
+    public void after() {
         sqlSession.close();
     }
 
--- MyBatis/src/main/java/io/kanonair/mapper/UserMapper.java
+++ MyBatis/src/main/java/io/kanonair/mapper/UserMapper.java
@@ -1,9 +1,23 @@
 package io.kanonair.mapper;
 
 import io.kanonair.pojo.User;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
 
 public interface UserMapper {
-    List<User> selectAll();
+    User selectById(Integer id);
+
+    List<User> selectByNameAndSex(@Param("name") String name, @Param("sex") Integer sex);
+
+    List<User> selectByNameAndSexMap(Map<String, String> map);
+
+    void update(User user);
+
+    List<User> selectByUserTwo(User u1, User u2);
+
+    List<User> selectLikeName(String name);
+
+    void insert(User user);
 }
