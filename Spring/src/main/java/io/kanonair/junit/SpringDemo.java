50a9e25662d98f6c79688591a356b9cf6f838b66
kanonair <15954027738@163.com>
2019-10-18 00:28:42 +0800

    init

--- Spring/src/main/java/io/kanonair/junit/SpringDemo.java
+++ Spring/src/main/java/io/kanonair/junit/SpringDemo.java
@@ -1,8 +1,11 @@
 package io.kanonair.junit;
 
+import com.zaxxer.hikari.HikariDataSource;
 import io.kanonair.pojo.Person;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.sql.Connection;
 
 public class SpringDemo {
 
@@ -12,6 +15,18 @@ public class SpringDemo {
                 new ClassPathXmlApplicationContext("applicationContext.xml");
         Person person = ioc.getBean(Person.class);
         System.out.println(person);
+        ioc.close();
+    }
+
+    @Test
+    public void example02() throws Exception {
+        ClassPathXmlApplicationContext ioc =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+        HikariDataSource dataSource = ioc.getBean(HikariDataSource.class);
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+        connection.close();
+        dataSource.close();
         ioc.close();
     }
 
