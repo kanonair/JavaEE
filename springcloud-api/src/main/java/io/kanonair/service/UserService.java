f04c1327b2e75e44ac3a6b29ef2da4e68109db59
kanonair <15954027738@163.com>
2019-10-28 00:20:07 +0800

    init

--- springcloud-api/src/main/java/io/kanonair/service/UserService.java
+++ springcloud-api/src/main/java/io/kanonair/service/UserService.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-@FeignClient("springcloud-provider")
+@FeignClient("SPRINGCLOUD-PROVIDER")
 public interface UserService {
     @GetMapping("list")
     List<User> list();
